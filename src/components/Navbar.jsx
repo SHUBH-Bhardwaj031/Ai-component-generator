d428286af69595bfdc987ada2f370b605637a51b
Shubham Bhardwaj <shubhambhardwaj10oct@gmail.com>
2026-01-04 02:22:38 +0530

    2nd Day

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaUser } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import { MdSunny } from "react-icons/md";
 
 const Navbar = () => {
   return (
@@ -6,6 +9,11 @@ const Navbar = () => {
       <div className="flex w-full justify-between px-[70px] items-center h-[100px] border-b-[2px] border-gray-800">
         <div className="logo">
             <h3 className='text-[25px] font-[700] sp-text'>GenUi</h3>
+        </div>
+        <div className="icons flex gap-4">
+          <div className="icon"><MdSunny /></div>
+          <div className="icon"><FaUser /></div>
+          <div className="icon"><IoMdSettings /></div>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Select from 'react-select';
 
 const Home = () => {
+  const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' },
+];
   return (
     <div>
       <Navbar/>
-      Home
+    <div className='flex items-center justify-between p-[50px]  gap-[30px]'>
+      <div className="left  mt-3 h-[70vh] w-[50%] bg-[#141319] p-[20px]">
+        <h1 className='font-bold text-2xl sp-text'>Ai Component Generator</h1>
+        <p className='text-gray-400 py-2'>Describe Your Thought All Will Help..</p>
+   <Select
+  options={options}
+  theme={(theme) => ({
+    ...theme,
+    colors: {
+      ...theme.colors,
+      primary: "#6b7280",     // focus (gray)
+      primary25: "#374151",  // hover (dark gray)
+      neutral0: "#1f2937",   // background
+      neutral80: "#e5e7eb",  // text
+      neutral20: "#4b5563",  // border
+    },
+  })}
+/>
+      </div>
+      <div className="right mt-3 h-[70vh] w-[50%] bg-[#141319]"></div>
+    </div>
     </div>
   )
 }
